210ea4e6558191f915676197667fd653a900475f
moling <365024424@qq.com>
2017-01-28 08:21:55 +0800

    编写主要页面的路由

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div class="app">
+    <router-view></router-view>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import Main from 'components/main/main'
+import Free from 'components/free/free'
+import Boy from 'components/boy/boy'
+import Girl from 'components/girl/girl'
+import Category from 'components/category/category'
+import Book from 'components/book/book'
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
+Vue.use(VueRouter)
+
+let app = Vue.extend(App)
+
+let router = new VueRouter({
+  linkActiveClass: 'active'
 })
+
+router.map({
+  '/': { component: Main },
+  '/free': { component: Free },
+  '/boy': { component: Boy },
+  '/girl': { component: Girl },
+  '/category': { component: Category },
+  '/book': { component: Book }
+})
+
+router.start(app, '#app')
