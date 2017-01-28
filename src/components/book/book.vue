4784b3675d3b820267ff6b234b1100f2c524422d
moling <365024424@qq.com>
2017-01-28 17:13:36 +0800

    编写公用顶部组件

--- src/components/book/book.vue
+++ src/components/book/book.vue
@@ -1,14 +1,31 @@
 <template>
-  <div class="book">book page</div>
+  <div class="book">
+    <top :title="book.title"></top>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
   import { getApiData } from 'common/js/utils.js'
+  import Top from 'components/common/top/top'
 
   export default {
+    components: {
+      Top
+    },
+    data () {
+      return {
+        book: {},
+        author_books: [],
+        related: []
+      }
+    },
     ready () {
-      let id = '42078'
-      getApiData(`/api/book/${id}`, data => console.log(data))
+      let id = '348392'
+      getApiData(`/api/book/${id}`, data => {
+        this.book = data.item
+        this.author_books = data.author_books
+        this.related = data.related
+      })
     }
   }
 </script>
---
+++ src/components/common/top/top.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="top">
+    <a class="back"></a>
+    <span class="title" v-text="title"></span>
+    <a v-link="{path: '/'}" class="home"></a>
+  </header>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      title: String
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../../common/stylus/mixin.styl'
+
+  .top
+    position fixed
+    right 0
+    left 0
+    height 44px
+    border-bottom 1px solid #ddd
+    font-size 15px
+    line-height 44px
+    color rgba(0, 0, 0, .7)
+    background-color #efeff0
+  .back, .home
+    position absolute
+    width 44px
+    height 44px
+    cursor pointer
+    &:before
+      content ''
+      display inline-block
+      margin-top 14px
+  .back
+    left 0
+  .home
+    right 0
+  .back:before
+    margin-left 17px
+    bg-img('back.png', 10px, 16px)
+  .home:before
+    margin-left 14px
+    bg-img('home.png', 16px, 16px)
+
+  .title
+    margin-left 44px
+</style>
