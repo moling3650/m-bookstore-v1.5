a705b88a93e1abe3362f3da27e705a0b6caa8857
moling <365024424@qq.com>
2017-01-29 17:05:31 +0800

    在main页面组件导入book-table组件

--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -1,13 +1,39 @@
 <template>
-  <div class="main">main page</div>
+  <div class="main">
+    <div class="container">
+      <book-table v-if="items[1]" :books="items[1].data"></book-table>
+      <book-table v-if="items[5]" :books="items[5].data"></book-table>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
   import { getApiData } from 'common/js/utils.js'
+  import BookTable from 'components/common/book-table/book-table'
 
   export default {
+    components: {
+      BookTable
+    },
+    data () {
+      return {
+        items: []
+      }
+    },
     ready () {
-      getApiData('/api/channel/418', data => console.log(data))
+      getApiData('/api/channel/418', data => {
+        data.items.map(item => {
+          let _item = {
+            ad_name: item.ad_name,
+            hidden_info: item.hidden_info,
+            data: []
+          }
+          item.data.data.map(data => {
+            _item.data.push(data.data ? data.data : data)
+          })
+          this.items.push(_item)
+        })
+      })
     }
   }
 </script>
