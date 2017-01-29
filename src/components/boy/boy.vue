10945955335c6621f985782de3920b8056758382
moling <365024424@qq.com>
2017-01-29 19:32:19 +0800

    在boy/girl页面组件导入top/group-header组件

--- src/components/boy/boy.vue
+++ src/components/boy/boy.vue
@@ -1,13 +1,40 @@
 <template>
-  <div class="boy">boy page</div>
+  <div class="boy">
+    <top title="男生频道"></top>
+    <div class="container">
+      <section class="main-card" v-for="item in items">
+        <group-header :title="item.ad_name"></group-header>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
   import { getApiData } from 'common/js/utils.js'
+  import Top from 'components/common/top/top'
+  import GroupHeader from 'components/common/group-header/group-header'
 
   export default {
+    components: {
+      Top,
+      GroupHeader
+    },
+    data () {
+      return {
+        items: []
+      }
+    },
     ready () {
-      getApiData('/api/channel/369', data => console.log(data))
+      getApiData('/api/channel/369', data => {
+        data.items.map(item => {
+          let _item = {
+            ad_name: item.ad_name,
+            hidden_info: item.hidden_info,
+            data: item.data.data
+          }
+          this.items.push(_item)
+        })
+      })
     }
   }
 </script>
--- src/components/girl/girl.vue
+++ src/components/girl/girl.vue
@@ -1,13 +1,40 @@
 <template>
-  <div class="girl">girl page</div>
+  <div class="girl">
+    <top title="女生频道"></top>
+    <div class="container">
+      <section class="main-card" v-for="item in items">
+        <group-header :title="item.ad_name"></group-header>
+      </section>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
   import { getApiData } from 'common/js/utils.js'
+  import Top from 'components/common/top/top'
+  import GroupHeader from 'components/common/group-header/group-header'
 
   export default {
+    components: {
+      Top,
+      GroupHeader
+    },
+    data () {
+      return {
+        items: []
+      }
+    },
     ready () {
-      getApiData('/api/channel/370', data => console.log(data))
+      getApiData('/api/channel/370', data => {
+        data.items.map(item => {
+          let _item = {
+            ad_name: item.ad_name,
+            hidden_info: item.hidden_info,
+            data: item.data.data
+          }
+          this.items.push(_item)
+        })
+      })
     }
   }
 </script>
