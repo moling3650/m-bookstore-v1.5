04ce09200b5defa9c727fcaf85041382811c630b
moling <365024424@qq.com>
2017-01-30 07:09:17 +0800

    添加group-footer组件，并在boy页面组件测试

---
+++ src/components/common/group-footer/group-footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <a href="#" class="group-footer" v-if="text != undefined" v-text="text ? text : '查看更多'"></a>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      text: String
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .group-footer
+    display block
+    padding 10px 0
+    border-top 1px solid #f0f0f0
+    color #333
+    text-align center
+    text-decoration none
+    cursor pointer
+    outline none
+</style>
