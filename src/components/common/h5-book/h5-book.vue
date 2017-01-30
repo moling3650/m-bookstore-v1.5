b3899d40482efc753d544dd9481550908e2ba5af
moling <365024424@qq.com>
2017-01-30 14:22:56 +0800

    添加h5-book组件，并在main页面组件测试

---
+++ src/components/common/h5-book/h5-book.vue
@@ -0,0 +1,92 @@
+<template>
+  <li class="h5-book">
+    <div class="cover-wrap">
+      <img class="cover" :src="book.cover" :alt="book.title" />
+      <p class="finish" v-text="book.finish ? '完结' : '连载'"></p>
+    </div>
+    <div class="info">
+      <p class="title" v-text="book.title"></p>
+      <p class="author" v-text="book.authors"></p>
+      <p class="summary" v-text="book.summary"></p>
+      <p class="tags">
+        <span class="tag" v-for="tag in tags" v-text="tag"></span>
+      </p>
+    </div>
+  </li>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      book: Object
+    },
+    computed: {
+      tags () {
+        let _tags = this.book.tags.slice(0, 2)
+        _tags.push(parseInt(this.book.word_count / 10000) + '万字')
+        return _tags
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '../../../common/stylus/mixin'
+
+  .h5-book
+    display flex
+    overflow hidden
+  .cover-wrap
+    position relative
+    flex 0 0 85px
+    height 113px
+    .cover
+      width 100%
+      height 100%
+    .finish
+      position absolute
+      right 0
+      bottom 0
+      left 0
+      padding 25px 7px 6px 7px
+      font-size 10px
+      line-height 10px
+      color #fff
+      background linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .5))
+  .info
+    flex 1
+    margin-left 15px
+  .title
+    margin-top 6px
+    font-size 16px
+    line-height 17px
+    color rgba(0, 0, 0, .9)
+    single-line()
+  .author
+    margin-top 8px
+    font-size 12px
+    line-height 12px
+    color rgba(0, 0, 0, .7)
+    single-line()
+  .summary
+    margin-top 8px
+    height 2.8em
+    font-size 12px
+    line-height 1.4em
+    display -webkit-box
+    overflow hidden
+    text-overflow ellipsis
+    -webkit-line-clamp 2
+    -webkit-box-orient vertical
+  .tag
+    display inline-block
+    max-width 6em
+    padding 3px 6px 1px
+    border 1px solid
+    border-radius 3px
+    margin-right 6px
+    font-size 10px
+    line-height 11px
+    color #54ac7c
+    single-line()
+</style>
--- src/components/main/main.vue
+++ src/components/main/main.vue
@@ -6,6 +6,22 @@
         <book-table :books="items[1].data"></book-table>
         <group-footer :text="items[1].info.more_text"></group-footer>
       </section>
+      <section class="main-card" v-if="items[3]">
+        <header class="h5-header">
+          <h2 class="title" v-text="items[3].ad_name"></h2>
+        </header>
+        <ul class="h5-book-list">
+          <h5-book v-for="book in items[3].data | limitBy 5" :book="book"></h5-book>
+        </ul>
+      </section>
+      <section class="main-card" v-if="items[4]">
+        <header class="h5-header">
+          <h2 class="title" v-text="items[4].ad_name"></h2>
+        </header>
+        <ul class="h5-book-list">
+          <h5-book v-for="book in items[4].data | limitBy 5" :book="book"></h5-book>
+        </ul>
+      </section>
       <section class="main-card" v-if="items[5]">
         <group-header :title="items[5].ad_name"></group-header>
         <book-table :books="items[5].data"></book-table>
@@ -20,12 +36,14 @@
   import GroupHeader from 'components/common/group-header/group-header'
   import BookTable from 'components/common/book-table/book-table'
   import GroupFooter from 'components/common/group-footer/group-footer'
+  import H5Book from 'components/common/h5-book/h5-book'
 
   export default {
     components: {
       GroupHeader,
       BookTable,
-      GroupFooter
+      GroupFooter,
+      H5Book
     },
     data () {
       return {
@@ -53,4 +71,20 @@
 <style lang="stylus" scoped>
   .book-table
     padding 13px 14px 5px
+
+  .h5-header
+    padding 15px 13px 14px 13px
+    border-bottom 1px solid #f0f0f0
+    .title
+      font-size 13px
+      line-height 13px
+      font-weight bold
+      color rgba(0, 0, 0, .9)
+  .h5-book-list
+    padding 0 13px
+  .h5-book
+    padding 17px 0
+    border-bottom 1px solid #f0f0f0
+    &:last-child
+      border-bottom none
 </style>
