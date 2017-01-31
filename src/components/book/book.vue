afc5253db3dc0b3024c017f2d191a31b9e14ef92
moling <365024424@qq.com>
2017-01-31 19:53:50 +0800

    编写book页面组件的底部区域

--- src/components/book/book.vue
+++ src/components/book/book.vue
@@ -2,8 +2,24 @@
   <div class="book">
     <top :title="book.title"></top>
     <main class="container">
-      <book-table :books="author_books"></book-table>
-      <book-table :books="related"></book-table>
+      <div class="main-card">
+        <h2 class="tag-header">类别标签</h2>
+        <ul class="tags">
+          <li class="tag" v-for="tag in tags" v-text="tag"></li>
+        </ul>
+      </div>
+      <div class="main-card">
+        <h1 class="title">作者其他图书</h1>
+        <book-table :books="author_books"></book-table>
+      </div>
+      <div class="main-card">
+        <h1 class="title">喜欢本书的人也喜欢</h1>
+        <book-table :books="related"></book-table>
+      </div>
+      <div class="main-card">
+        <h1 class="title">图书信息</h1>
+        <p class="rights" v-text="book.rights"></p>
+      </div>
     </main>
   </div>
 </template>
@@ -17,6 +33,11 @@
     components: {
       Top,
       BookTable
+    },
+    computed: {
+      tags () {
+        return this.book.tags.slice(0, 3)
+      }
     },
     route: {
       canReuse () {
@@ -42,4 +63,42 @@
 </script>
 
 <style lang="stylus" scoped>
+  .tag-header
+    padding 14px 0 8px
+    font-size 16px
+    line-height 21px
+    font-weight bold
+  .tags
+    margin-bottom 10px
+    font-size 0
+    line-height 1.6
+    color #766d5d
+    .tag
+      display inline-block
+      width auto
+      padding 0 15px
+      border 1px solid #d3d3d3
+      border-radius 4px
+      margin-right 10px
+      margin-bottom 5px
+      font-size 14px
+      text-align center
+      for $color, $idx in #fbebe8 #fcedda #e8f9db
+        &:nth-child(3n+{$idx + 1})
+          background-color $color
+  .main-card
+    padding 0 14px 0 14px
+    color #8d8d8d
+    .title
+      padding-top 15px
+      padding-bottom 8px
+      font-size 15px
+    .rights
+      font-size 14px
+      line-height 1.6
+      &:before
+        content '版权：'
+
+
+
 </style>
